4e171f5b7b73fb6e638befa74614a35f4ffda47f
John DeLong <jdelong@michiganlabs.com>
2020-07-19 15:38:54 -0400

    Run commands in the background

--- docker/rtl_433/rtl_start.sh
+++ docker/rtl_433/rtl_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rtl_433 -f 319500000 -R 100 -C si -M newmodel -X "repeats>=4" -F "mqtt://${MQTT_IP}:1883,retain=1,events=rtl_433/events[/type][/model][/subtype][/channel][/id],user=${MQTT_USER},pass=${MQTT_PASSWORD}";
-python3 ha_discovery.py;
+rtl_433 -f 319500000 -R 100 -C si -M newmodel -X "repeats>=4" -F "mqtt://${MQTT_IP}:1883,retain=1,events=rtl_433/events[/type][/model][/subtype][/channel][/id],user=${MQTT_USER},pass=${MQTT_PASSWORD}" &
+python3 ha_discovery.py &
